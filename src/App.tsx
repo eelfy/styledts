7fc7f7063b7399d1b93a4f52a8073a2dbf312aaa
Nikita Tolstikov <n.tolstikov@digitarnia.ru>
2021-11-02 13:14:35 +0300

    frsty

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button from './components/Button/Button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const App:React.FC = ()=> (
+    <>
+        <Button 
+            width="150px" 
+            themeName="orangeMedium"
+            margin='30px'
+            marginBottom='0px'
+            active={true}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+            Button
+        </Button>
+    </>
+)
 
 export default App;
